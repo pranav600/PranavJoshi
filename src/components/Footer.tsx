29c821bf9b72d615867cdfba64afa51be995cded
pranav600 <pranavjoshitkd@gmail.com>
2026-01-18 15:55:28 +0530

    small change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+/**
+ * Footer component - Displays site footer with branding
+ */
 const Footer = () => {
   return (
     <footer className="bg-gray-100 dark:bg-[#181818]/90 py-8 px-4 backdrop-blur border-t border-gray-200 dark:border-white/5">
